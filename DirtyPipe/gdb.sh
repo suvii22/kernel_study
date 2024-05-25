8ea01be56f9d8628dbd800943fb3cf79da889a46
lvyzh <45846919+suvii22@users.noreply.github.com>
2024-05-25 14:36:02 +0800

    Update gdb.sh

--- DirtyPipe/gdb.sh
+++ DirtyPipe/gdb.sh
@@ -2,5 +2,6 @@ gdb \
     -ex "add-auto-load-safe-path /home/lvyzh/kernel/" \
     -ex "file /home/lvyzh/kernel/vmlinux" \
     -ex "target remote localhost:1234" \
-    -ex "b do_sys_openat2" \
+    -ex "source page.py" \
+    -ex "dprintf *(0xffffffff81208fdf), \"alloc pipe info  0x%llx\n\", \$rax" \
     -ex "c"
